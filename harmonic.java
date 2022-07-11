7aea40fe1d049f53518cc6d5ad83585c3eba539f
unknown <annapureddy738@gmail.com>
2022-07-11 15:58:46 +0530

    harmonic number

---
+++ harmonic.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class harmonic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter any number : ");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        System.out.print("The Harmonic Series is : ");
+        double result = 0.0;
+        while(num > 0)
+          {
+               result = result + (double) 1 / num;
+               num--;
+               System.out.print(result +"  ");
+          }
+        System.out.println("");
+        System.out.println("Output of Harmonic Series is "+result);
+	}
+}
